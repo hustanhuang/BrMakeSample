c2ccd5f5f8d0ee024d17f375c5eda17c5789a95a
andyroiiid <andyroiiid@live.com>
2018-02-28 17:14:35 +0800

    basic unity command line build

---
+++ Assets/BrMake/Builder.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace BrMake
+{
+    public class Builder : MonoBehaviour
+    {
+        private static void BuildIos()
+        {
+            var options = new BuildPlayerOptions
+            {
+                scenes = new[]
+                {
+                    "Assets/Main.unity"
+                },
+                locationPathName = Directory.GetCurrentDirectory() + "/Build/stage1",
+                target = BuildTarget.iOS
+            };
+
+            BuildPipeline.BuildPlayer(options);
+        }
+    }
+}
+#endif
---
+++ build.py
@@ -0,0 +1,1 @@
+/Users/andyroiiid/BrMake/main.py
